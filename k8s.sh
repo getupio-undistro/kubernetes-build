b155e014aac6164e5caa2da8a00953d4cfdac9b0
Felipe Oliveira <fpo@felipeweb.dev>
2021-06-14 23:48:17 +0000

    compile k8s

---
+++ k8s.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+KUBERNETES_VERSION=$1
+
+git clone https://github.com/kubernetes/kubernetes.git
+cd kubernetes
+git checkout tags/${KUBERNETES_VERSION}
+VERSION_PRERELEASE=undistro KUBE_DOCKER_REGISTRY=docker.io/getupioundistro KUBE_RELEASE_RUN_TESTS=n make release
