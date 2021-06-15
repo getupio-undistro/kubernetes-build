e21d3fca01cbad77fea3630e3c4ea5b830a38e84
Felipe Oliveira <fpo@felipeweb.dev>
2021-06-15 17:46:54 -0300

    install go
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- k8s.sh
+++ k8s.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
 KUBERNETES_VERSION=$1
-
+rm -rf kubernetes
 git clone https://github.com/kubernetes/kubernetes.git
 cd kubernetes
 git checkout tags/${KUBERNETES_VERSION}
-VERSION_PRERELEASE=undistro KUBE_DOCKER_REGISTRY=docker.io/getupioundistro KUBE_RELEASE_RUN_TESTS=n make cross
-echo 'MAKE RELEASE'
-VERSION_PRERELEASE=undistro KUBE_DOCKER_REGISTRY=docker.io/getupioundistro KUBE_RELEASE_RUN_TESTS=n make release
+VERSION_PRERELEASE=undistro KUBE_RELEASE_RUN_TESTS=n make release
