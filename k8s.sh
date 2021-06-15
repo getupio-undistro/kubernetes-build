901bb42d0d99739510a3d549d685fc85796c8412
Felipe Oliveira <fpo@felipeweb.dev>
2021-06-15 15:45:40 -0300

    try clean
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- k8s.sh
+++ k8s.sh
@@ -5,4 +5,5 @@ KUBERNETES_VERSION=$1
 git clone https://github.com/kubernetes/kubernetes.git
 cd kubernetes
 git checkout tags/${KUBERNETES_VERSION}
-VERSION_PRERELEASE=undistro KUBE_RELEASE_RUN_TESTS=n make release
+VERSION_PRERELEASE=undistro KUBE_DOCKER_REGISTRY=docker.io/getupioundistro KUBE_RELEASE_RUN_TESTS=n make clean
+VERSION_PRERELEASE=undistro KUBE_DOCKER_REGISTRY=docker.io/getupioundistro KUBE_RELEASE_RUN_TESTS=n make release
