bdf2972eacbe296e6c33376bd7cca1d169cce55d
Felipe Oliveira <fpo@felipeweb.dev>
2021-06-15 18:38:56 -0300

    try prune
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- k8s.sh
+++ k8s.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 KUBERNETES_VERSION=$1
+docker system prune -fa
 rm -rf kubernetes
 git clone https://github.com/kubernetes/kubernetes.git
 cd kubernetes
 git checkout tags/${KUBERNETES_VERSION}
-VERSION_PRERELEASE=undistro KUBE_RELEASE_RUN_TESTS=n make release
+VERSION_PRERELEASE=undistro KUBE_DOCKER_REGISTRY=docker.io/getupioundistro KUBE_RELEASE_RUN_TESTS=n make release
